c0cd458dab77217773b1a2e4e2931ea29a27c791
Jesse Evers <jesse@jesseevers.com>
2019-12-10 16:14:37 +0200

    Added documentation to ht_* methods, changed lib/hash_table.h to regular file

--- projects/06/hashtable/src/hash_table.c
+++ projects/06/hashtable/src/hash_table.c
@@ -34,6 +34,7 @@ static void ht_del_item(ht_item **i) {
 
 /**
  * Creates a new linked list of `ht_item`s.
+ * 
  * @return a pointer to a new, empty linked list
  */
 ll_node *ll_new() {
@@ -42,10 +43,11 @@ ll_node *ll_new() {
 
 /**
  * Adds an item to the head of the given linked list
- * @param  list  the linked list to add to
- * @param  key   the key to add to the linked list
- * @param  value the value to add to the linked list
- * @return       a pointer to the new head of the linked list
+ * 
+ * @param list  the linked list to add to
+ * @param key   the key to add to the linked list
+ * @param value the value to add to the linked list
+ * @return      a pointer to the new head of the linked list
  */
 ll_node *ll_insert(ll_node *list, const char *key, const char *value) {
     ll_node *new_node = calloc(1, sizeof(ll_node));
@@ -55,11 +57,12 @@ ll_node *ll_insert(ll_node *list, const char *key, const char *value) {
 }
 
 /**
- * Searches recursively for a specific `ht_item` key in a linked list
- * @param  key     the key to search for
- * @param  current the current linked list node being recursed over
- * @param  prev    the last linked list node that was recursed over
- * @return         the corresponding value to `key`, or NULL
+ * Searches recursively for a specific `ht_item` key in a linked list.
+ * 
+ * @param key     the key to search for
+ * @param current the current linked list node being recursed over
+ * @param prev    the last linked list node that was recursed over
+ * @return        the corresponding value to `key`, or NULL
  */
 static char *ll_search_recur(const char *key, const ll_node *current, const ll_node *prev) {
     if (current == &LL_SENTINEL) {
@@ -72,10 +75,11 @@ static char *ll_search_recur(const char *key, const ll_node *current, const ll_n
 }
 
 /**
- * Searches a linked list of `ht_item`s for the given key
- * @param  node the linked list to search
- * @param  key  the key to search for
- * @return      the value corresponding to `key`, or NULL
+ * Searches a linked list of `ht_item`s for the given key.
+ * 
+ * @param node the linked list to search
+ * @param key  the key to search for
+ * @return     the value corresponding to `key`, or NULL
  */
 char *ll_search(ll_node *node, const char *key) {
     return ll_search_recur(key, node, NULL);
@@ -101,7 +105,8 @@ static int ll_remove_recur(const char *key, ll_node **current, ll_node *prev) {
 }
 
 /**
- * Removes the node from the linked list of `ht_item`s whose key is `key`
+ * Removes the node from the linked list of `ht_item`s whose key is `key`.
+ * 
  * @param node the linked list to remove a node from
  * @param key  the key to remove
  */
@@ -110,7 +115,8 @@ int ll_remove(ll_node **node, const char *key) {
 }
 
 /**
- * Deletes the given linked list
+ * Deletes the given linked list.
+ * 
  * @param node the linked list to delete
  */
 void ll_delete(ll_node **node) {
@@ -125,9 +131,10 @@ void ll_delete(ll_node **node) {
 /**** HASH TABLE FUNCTIONS ****/
 
 /**
- * Computes the FNV1a hash of the given input
- * @param  input the value to hash
- * @return       the hashed value of the input
+ * Computes the FNV1a hash of the given input.
+ * 
+ * @param input the value to hash
+ * @return      the hashed value of the input
  */
 unsigned long long fnv1a(const char *input) {
     unsigned long long hash = HT_FNV_OFFSET_BASIS;
@@ -142,6 +149,12 @@ static unsigned long long ht_hash(const char *input, const int size) {
     return fnv1a(input) % size;
 }
 
+/**
+ * Creates a new hash table, with the given number of lists.
+ * 
+ * @param size the size of the hash table
+ * @return     the new hash table
+ */
 ht_hash_table *ht_new(const int size) {
     ht_hash_table* ht = calloc(1, sizeof(ht_hash_table));
     ht->size = size;
@@ -153,12 +166,30 @@ ht_hash_table *ht_new(const int size) {
     return ht;
 }
 
+/**
+ * Inserts the given key/value pair into the hash table.
+ * 
+ * @param ht  the hash table to insert into
+ * @param key the key to insert
+ * @param val the value to insert
+ */
 void ht_insert(ht_hash_table *ht, const char *key, const char *val) {
     unsigned long long hash = ht_hash(key, ht->size);
     ht->nodes[hash] = ll_insert(ht->nodes[hash], key, val);
     ht->count++;
 }
 
+/**
+ * Inserts multiple key/value pairs, pairing each item in the list of keys with the item in the list of values at
+ * the same index. There must be the same number of keys and values, and there must be ae NULL sentinel value at the
+ * end of both lists.
+ * 
+ * @param ht        the hash table to insert into
+ * @param num_items the number of items to insert. NOTE: the NULL sentinel value at the end of each list doesn't count
+ *                  towards this value
+ * @param keys      the list of keys to insert, ending with a NULL sentinel value
+ * @param vals      the list of values to insert relative to the keys, ending with a NULL sentinel value
+ */
 void ht_insert_all(ht_hash_table *ht, int num_items, const char *keys[], const char *vals[]) {
     const char *init_items_err =
         "[ERR] The list of keys and the list of values supplied to ht_insert_all() must both be non-NULL and "
@@ -179,18 +210,36 @@ void ht_insert_all(ht_hash_table *ht, int num_items, const char *keys[], const c
     }
 }
 
+/**
+ * Searches the hash table for a value corresponding to the given key.
+ * 
+ * @param ht  the hash table to search
+ * @param key the key to search for
+ * @return    the value corresponding to the given key if one exists, NULL otherwise
+ */
 char *ht_search(ht_hash_table *ht, const char *key) {
     unsigned long long hash = ht_hash(key, ht->size);
     ll_node *node = ht->nodes[hash];
     return ll_search(node, key);
 }
 
+/**
+ * Removes the key/value pair corresponding to the given key from the hash table, if it exists.
+ * 
+ * @param ht  the hash table to remove the key/value pair from
+ * @param key removes the key/value pair corresponding to this key, if one exists
+ */
 void ht_remove(ht_hash_table *ht, const char *key) {
     unsigned long long hash = ht_hash(key, ht->size);
     int removed = ll_remove(&(ht->nodes[hash]), key);
     if (removed) ht->count--;
 }
 
+/**
+ * Deletes the hash table, and all key/value pairs in it.
+ *
+ * @param ht the hash table to delete
+ */
 void ht_delete(ht_hash_table *ht) {
     for (int i = 0; i < ht->size; i++) {
         ll_delete(&(ht->nodes[i]));
--- projects/lib/hash_table.h
+++ projects/lib/hash_table.h
@@ -1,1 +1,46 @@
-/home/jesse/src/hubg/nand2tetris/projects/06/hashtable/src/hash_table.h
+#ifndef _HASH_TABLE_H
+#define _HASH_TABLE_H
+
+#include <stdlib.h>
+
+// A key/value pair in a hash table
+typedef struct ht_item {
+    char* key;
+    char* value;
+} ht_item;
+
+// A node in a linked list of hash table items
+typedef struct ll_node {
+    ht_item *value;
+    struct ll_node *next;
+} ll_node;
+
+// A hash table
+typedef struct ht_hash_table {
+    int size;
+    int count;
+    ll_node **nodes;
+} ht_hash_table;
+
+extern ll_node LL_SENTINEL;
+
+static const unsigned long long HT_FNV_OFFSET_BASIS = 0xCBF29CE484222325U;
+static const unsigned long long HT_FNV_PRIME = 0x100000001B3U;
+
+/* Linked list functions */
+ll_node *ll_new();
+ll_node *ll_insert(ll_node*, const char*, const char*);
+char *ll_search(ll_node*, const char*);
+int ll_remove(ll_node**, const char*);
+void ll_delete(ll_node**);
+
+/* Hash table functions */
+unsigned long long fnv1a(const char*);
+ht_hash_table *ht_new(int);
+void ht_insert_all(ht_hash_table*, int, const char**, const char**);
+void ht_insert(ht_hash_table*, const char*, const char*);
+char *ht_search(ht_hash_table*, const char*);
+void ht_remove(ht_hash_table*, const char*);
+void ht_delete(ht_hash_table*);
+
+#endif
