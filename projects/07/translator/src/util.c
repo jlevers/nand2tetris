480f1444048c6d36d9775fedc49e93691c64d3ef
Jesse Evers <jesse@jesseevers.com>
2019-12-10 22:49:43 +0200

    Added toupper_str and num_digits to util.[ch]

--- projects/07/translator/src/util.c
+++ projects/07/translator/src/util.c
@@ -1,13 +1,59 @@
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 
 /**
  * Checks if the given path is a directory.
  * 
- * @param  char*  path  The path to check
- * @return int          1 if the path is a directory, 0 otherwise
+ * @param path the path to check
+ * @return     1 if the path is a directory, 0 otherwise
  */
 int is_directory(const char *path) {
     struct stat path_stat;
     stat(path, &path_stat);
     return S_ISDIR(path_stat.st_mode);
+}
+
+
+/**
+ * Converts the given string to uppercase.
+ *
+ * @param dest the string to write the uppercase string into
+ * @param str  the string to convert
+ * @return     the string in uppercase
+ */
+char *toupper_str(char *dest, const char *str) {
+    int str_len = strlen(str);
+
+    int i;
+    for (i = 0; i < str_len; i++) {
+        dest[i] = toupper(str[i]);
+    }
+    dest[i] = '\0';
+
+    return dest;
+}
+
+
+/**
+ * Counts the number of digits in the given integer.
+ *
+ * @param num the number whose number of digits is to be counted
+ * @return    the number of digits in num
+ */
+int num_digits(const int num) {
+    int copy = num;
+    int count = 0;
+
+    if (copy <= 0) {
+        count++;
+        copy = -1 * copy;
+    }
+
+    while (copy > 0) {
+        count++;
+        copy /= 10;
+    }
+    return count;
 }
--- projects/07/translator/src/util.h
+++ projects/07/translator/src/util.h
@@ -2,5 +2,7 @@
 #define _VM_UTIL_H
 
 int is_directory(const char*);
+char toupper_str(char*, const char*);
+int num_digits(int);
 
 #endif
